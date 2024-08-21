f1b3b5f85278f4e1ec558a435f51ac470dc921ca
Jimmy <169525180+jimmyKodar@users.noreply.github.com>
2024-08-21 21:35:02 +0200

    none

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -10,8 +10,8 @@ const NewsItem = ({
 }) => {
   return (
     <article className="newsItem">
-      <h1 className="headline">{headline}</h1>
-      <div className="tickers">
+      <span className="timestamp">{created_at.slice(11, -1)}</span>
+      <span>
         {symbols.map((ticker) => {
           return (
             <span key={ticker} className={`ticker ${highlight()}`}>
@@ -19,24 +19,36 @@ const NewsItem = ({
             </span>
           );
         })}
-      </div>
-      <small className="newsItemInfo">
-        ID: {id} / Created: {created_at} / Updated: {updated_at} / Author:{" "}
-        {author} / Source:{" "}
-        <a href={url} target="_blank" rel="noopener noreferrer">
-          {source}
-        </a>
-      </small>
+
+        <span className="headline">{headline}</span>
+        {
+          <small className="newsItemInfo">
+            ID: {id} / Created: {created_at} / Updated: {updated_at} / Author:{" "}
+            {author} / Source:{" "}
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              {source}
+            </a>
+          </small>
+        }
+      </span>
     </article>
   );
 };
 
-function highlight() {
+const highlight = () => {
   if (Math.random() * 100 > 70) {
     return "tickerHighlight";
   } else {
     return "";
   }
-}
+};
+
+const timestampNow = () => {
+  let now = new Date();
+  let currentTimeHours = (now.getHours() - 6).toString().padStart(2, 0);
+  let mincurrentTimeMinutes = now.getMinutes().toString().padStart(2, 0);
+  let mincurrentTimeSeconds = now.getSeconds().toString().padStart(2, 0);
+  return `${currentTimeHours}:${mincurrentTimeMinutes}:${mincurrentTimeSeconds}`;
+};
 
 export default NewsItem;
